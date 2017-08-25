56c518c3beff264872e85b98f2e502d8f914ba88
Cristrian Traina <crissstian96@gmail.com>
2017-08-25 23:55:52 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WebsiteModule } from 'app/website/website.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebsiteModule
   ],
   imports: [
     BrowserModule
---
+++ src/app/website/website.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebsiteComponent } from './website.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WebsiteComponent]
+})
+export class WebsiteModule { }
